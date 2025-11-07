29b8b4b60a9786a8d4db39a180a358927836ddc3
theamazingmrb <billie.heidelberg.jr@gmail.com>
2025-11-06 17:44:29 -0800

    urls

--- backend/settings.py
+++ backend/settings.py
@@ -31,8 +31,8 @@ if DATABASE_URL:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'pennypal_db',
         }
     }
 
